0d76d85449e4598ee929092cf0a2d32e0735a70a
Luigi Minopoli <luigi.minopoli.3d@gmail.com>
2025-12-29 13:01:36 +0100

    chore: + libvirtmanager

--- scripts/packages-install.sh
+++ scripts/packages-install.sh
@@ -1,12 +1,15 @@
 #!/bin/bash
 # Base packages installer
 
-
 sudo pacman -Syyu --noconfirm go btrfs-assistant visual-studio-code-bin telegram-desktop rsync nano flatpak
-
 yay -Syy --noconfirm hyprmon-bin brave-bin microsoft-edge-stable-bin
 
 # Add telegram as floating
 echo '' >> ~/.config/hypr/hyprland.conf
 echo 'windowrule = float, class:org.telegram.desktop' >> ~/.config/hypr/hyprland.conf
 echo 'windowrule = size 400 600, class:org.telegram.desktop' >> ~/.config/hypr/hyprland.conf
+
+# Installing virtualization-manager
+sudo pacman -S --needed qemu-full virt-manager libvirt dnsmasq ebtables
+sudo systemctl enable --now libvirtd
+sudo usermod -aG libvirt $(whoami)
