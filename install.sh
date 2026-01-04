dfcd283c0451001d8a69fbf1f65b4b435b6d9464
Luigi Minopoli <luigi.minopoli.3d@gmail.com>
2026-01-04 11:04:02 +0100

    chore: hypridle

--- install.sh
+++ install.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
-# First install only
 
-./scripts/bash-config.sh
+# Configure bash with https://gist.github.com/3ayazaya/d87c70c5f30a6e28f15dfc84ca95fc68
+cp -r .config/* ~/.config/
+
+# Configure hypridle for more time before sleep
+cp -r .config/hypr/hypridle.conf ~/.config/hypr/hypridle.conf
+
 ./scripts/snapper-config.sh
 ./scripts/package-install.sh
 ./scripts/ssh.sh
--- scripts/bash-config.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-# Configure bash with https://gist.github.com/3ayazaya/d87c70c5f30a6e28f15dfc84ca95fc68
-
-cp -r .config/* ~/.config/
